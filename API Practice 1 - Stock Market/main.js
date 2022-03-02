05b579a97e5e30699da3e96430254b5341fe0c2d
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-03-02 14:12:08 +0000

    JS functionality. Fetch Api and display in document
    Take symbol from the input box. Use this symbol in the api url to fetch data. Convert to json and assign to variables, to be displayed in the output. Added green text for increase and red for decrease.

---
+++ API Practice 1 - Stock Market/main.js
@@ -0,0 +1,77 @@
+// declare variables
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+// IMPORTANT - Get your own free API token from https://www.stockdata.org/
+const apiToken = 'N9cbbcb2mG2PtsxzoKTbfAp7DY1HwBoCq5R8sy2O';
+
+// aysnc fucntion to fetch stock data from API
+async function getStockData(){
+    // take symbol from the input field
+    const symbol = document.getElementById("input").value;
+    console.log(`symbol is ${symbol}`);
+
+    // declare the url using api link, symbol and token
+    let apiUrl = "https://api.stockdata.org/v1/data/quote?symbols="+ symbol +"&api_token=" + apiToken;
+    console.log(`apiUrl is ${apiUrl}`);
+    
+    try{
+        // return the fetched promise as json
+        const response = await fetch(apiUrl);
+        // convert response to json format
+        const data = await response.json();
+        // extract and name variables from response
+        let stock = data["data"][0];
+        let name = stock["name"];
+        let ticker = stock["ticker"];
+        let exchange = stock["exchange_short"];
+        let price = stock["price"];
+        let currency = stock["currency"];
+        let previous = stock["previous_close_price"];
+        let change = stock["day_change"];
+    
+        // show results in console log
+        console.log(data);
+        console.log(response);
+        console.log(data);
+        console.log(stock);
+        console.log(`name is ${name}`);
+        console.log(`ticker is ${ticker}`);
+        console.log(`exchange is ${exchange}`);
+        console.log(`price is ${price} ${currency}`);
+        console.log(`previous is ${previous} ${currency}`);
+        console.log(`change is ${change}`);
+
+        // results to be displayed
+        const result = `
+        <p>Ticker: ${ticker}</p>
+        <p>Name: ${name}</p>
+        <p>Exchange: ${exchange}</p>
+        <p>Price: <span id="price">${price}</span> ${currency}</p>
+        <p>Previous price: ${previous} ${currency}</p>
+        <p>Change: <span id="change">${change}%</span></p>
+        `;
+
+        // display results in document
+        output.innerHTML = result;
+
+        // make text green if increase, and red if decrease
+        if (change > 0){
+            document.getElementById("price").classList.add("green-day");
+            document.getElementById("change").classList.add("green-day");
+        } else if ( change < 0){
+            document.getElementById("price").classList.add("red-day");
+            document.getElementById("change").classList.add("red-day");
+        }
+
+    } catch(e){
+        console.log("Error!");
+        console.log(e);
+        // ask for a valid symbol if not fetch fails on that symbol
+        output.innerHTML = `<p>${symbol} not found. Please enter a valid symbol.</p>`;
+    }
+};
+
+// run function when button is clicked
+btn.addEventListener("click", function(){
+    getStockData();
+}); 
